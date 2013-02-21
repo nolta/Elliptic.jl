92267c17ba1aa590631a1d4129ef4603c6edfa4f
Mike Nolta <mike@nolta.net>
2013-02-20 21:58:02 -0500

    minor comment fix

--- src/test.jl
+++ src/test.jl
@@ -30,14 +30,13 @@ E20 = 1.52379_92052_59774
 @test_approx_eq_eps E(m20) E20 1e-15
 
 # values from Abramowitz & Stegun, Table 17.5 (p613-615)
+# values from Abramowitz & Stegun, Table 17.6 (p616-618)
 E2020 = 0.34825_492
 F2020 = 0.34988_016
 for i = -2:2
     @test_approx_eq_eps E(degrees2radians(20+180i), m20) E2020 + 2i*E20 1e-8
     @test_approx_eq_eps F(degrees2radians(20+180i), m20) F2020 + 2i*K20 1e-8
 end
-
-# values from Abramowitz & Stegun, Table 17.6 (p616-618)
 
 # values from Abramowitz & Stegun, Table 17.9 (p625-626)
 @test_approx_eq_eps Pi(0.2, degrees2radians(30), sind(30)^2) 0.53896 1e-5
