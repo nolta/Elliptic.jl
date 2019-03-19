9311f1d35971369c4790a0a5307e9f1e7f845f3a
Hamza El-Saawy <hamza.elsaawy@gmail.com>
2019-03-19 00:52:12 -0400

    Tests pass, inaccuracies as ϵ → 90°
    If u = K (ϵ = 90°) then `pc(K|m)` functions will be inaccurate since `cn` may not return
    exactly 0, so result will not be infinity, but ~±1e16.
    
    For some reason, `ellipj` is fairly accurate overall?
    
    OCR turned a 1 to an l

--- test/jacobi_tests.jl
+++ test/jacobi_tests.jl
@@ -3,9 +3,6 @@
 #
 
 @testset "Jacobi" begin
-    using Jacobi
-    using DelimitedFiles: readdlm
-
     @testset "Abramowitz & Stegun Table 16.1" begin
         # table 17.2
         αs_17_2 = readdlm("data/table_17_2/alpha.csv")
@@ -18,115 +15,99 @@
         θss = readdlm("data/table_16_1/theta_s.csv", ',')
         θns = readdlm("data/table_16_1/theta_n.csv", ',')
 
-       @testset "α = $α" for (i, α) in enumerate(αs)
-            @testset "ϵ = $ϵ" for (j, ϵ) in enumerate(ϵs)
-                ϵ₁ = 90 - ϵ
-                j₁ = length(ϵs) - i + 1
+        @testset "α = $α" for (i, α) in enumerate(αs)
+            K = Ks[αs_17_2_lut[α]]
+            m = sind(α)^2
+            denom = sqrt(secd(α))
 
-                m = sind(α)^2
-                K = Ks[αs_17_2_lut[α]]
+            @testset "ϵ = $ϵ" for (j, ϵ) in enumerate(ϵs)
+                j₁ = length(ϵs) - j + 1
+                ϵ₁ = ϵs[j₁]
                 u = ϵ * K / 90
-                denom = sqrt(secd(α))
 
                 θs = θss[j, i]
                 θn = θns[j, i]
                 θc = θss[j₁, i]/denom
                 θd = θns[j₁, i]/denom
 
-                @test sn(u, m) ≈ θs/θn atol=1e-9
-                @test cn(u, m) ≈ θc/θn atol=1e-9
-                @test dn(u, m) ≈ θd/θn atol=1e-9
-                @test nn(u, m) ≈ 1.0
+                @test Jacobi.sn(u, m) ≈ θs / θn atol=2.5e-9
+                @test Jacobi.cn(u, m) ≈ θc / θn atol=2.5e-9
+                @test Jacobi.dn(u, m) ≈ θd / θn atol=2.5e-9
+                @test Jacobi.nn(u, m) == 1.0
+
+                @test Jacobi.sd(u, m) ≈ θs / θd atol=2.5e-9
+                @test Jacobi.cd(u, m) ≈ θc / θd atol=2.5e-9
+                @test Jacobi.dd(u, m) == 1.0
+                @test Jacobi.nd(u, m) ≈ θn / θd atol=2.5e-9
+
+                @test Jacobi.cc(u, m) == 1.0
+                if ϵ != 90
+                    # very sensitive around u = K,
+                    # estimate of K(0) = pi/2 + 4e-9, so cosine causes errors
+                    # also, errors build up in ϵ ≥75°, so lower tolerences for that
+                    @test Jacobi.sc(u, m) ≈ θs / θc atol=1e-7
+                    @test Jacobi.dc(u, m) ≈ θd / θc atol=1e-7
+                    @test Jacobi.nc(u, m) ≈ θn / θc atol=1e-7
+                end
+
+                @test Jacobi.ss(u, m) == 1.0
+                @test Jacobi.cs(u, m) ≈ θc / θs atol=5.5e-8
+                @test Jacobi.ds(u, m) ≈ θd / θs atol=5.5e-8
+                @test Jacobi.ns(u, m) ≈ θn / θs atol=5.5e-8
+
+                # ellipj
+                s, c, d = ellipj(u, m)
+                @test s ≈ θs / θn atol=1e-9
+                @test c ≈ θc / θn atol=1e-9
+                @test d ≈ θd / θn atol=1e-9
             end
         end
     end
 
-    @testset "Abramowitz & Stegun, Table 16.1 (p582-583)" begin
-        # from Abramowitz & Stegun, Table 17.2 (p610-611)
-        m20 = sind(20)^2
-        K20 = 1.62002_58991_24204
-        E20 = 1.52379_92052_59774
-
-        u20 = 20*K20/90
-        θs2020 = 0.35274_9211
-        θc2020 = 0.96935_0025/sqrt(secd(20))
-        θd2020 = 1.02789_45992/sqrt(secd(20))
-        θn2020 = 1.00369_53131
-        # sn, cn, dn, nn
-        @test sn(u20,m20) ≈ θs2020/θn2020 atol=1e-9
-        @test cn(u20,m20) ≈ θc2020/θn2020 atol=1e-9
-        @test dn(u20,m20) ≈ θd2020/θn2020 atol=1e-9
-        @test nn(u20, m20) ≈ 1.0
-
-        # cd, sd, nd, dd
-        @test cd(u20,m20) ≈ θc2020/θd2020 atol=1e-9
-        @test sd(u20,m20) ≈ θs2020/θd2020 atol=1e-9
-        @test nd(u20,m20) ≈ θn2020/θd2020 atol=1e-9
-        @test dd(u20, m20) ≈ 1.0
-
-        # dc, nc, sc, cc
-        @test dc(u20,m20) ≈ θd2020/θc2020 atol=1e-9
-        @test nc(u20,m20) ≈ θn2020/θc2020 atol=1e-9
-        @test sc(u20,m20) ≈ θs2020/θc2020 atol=1e-9
-        @test cc(u20, m20) ≈ 1.0
-
-        # ns, ds, cs, ss
-        @test ns(u20,m20) ≈ θn2020/θs2020 atol=4e-9
-        @test ds(u20,m20) ≈ θd2020/θs2020 atol=4e-9
-        @test cs(u20,m20) ≈ θc2020/θs2020 atol=4e-9
-        @test ss(u20, m20) ≈ 1.0
-
-        # ellipj
-        s,c,d = ellipj(u20,m20)
-        @test s ≈ θs2020/θn2020 atol=1e-9
-        @test c ≈ θc2020/θn2020 atol=1e-9
-        @test d ≈ θd2020/θn2020 atol=1e-9
-    end
-
     @testset "u = $u" for u in -1.:0.21:1.0
-        @test am(u,0) ≈ u
-        @test sn(u,0) ≈ sin(u)
-        @test cn(u,0) ≈ cos(u)
-        @test dn(u,0) ≈ 1.
-        @test nn(u,0) ≈ 1.
-        @test cd(u,0) ≈ cos(u)
-        @test sd(u,0) ≈ sin(u)
-        @test nd(u,0) ≈ 1.
-        @test dd(u,0) ≈ 1.
-        @test dc(u,0) ≈ sec(u)
-        @test nc(u,0) ≈ sec(u)
-        @test sc(u,0) ≈ tan(u)
-        @test cc(u,0) ≈ 1.
-        @test ns(u,0) ≈ csc(u)
-        @test ds(u,0) ≈ csc(u)
-        @test cs(u,0) ≈ cot(u)
-        @test ss(u,0) ≈ 1.
+        @test Jacobi.am(u,0) ≈ u
+        @test Jacobi.sn(u,0) ≈ sin(u)
+        @test Jacobi.cn(u,0) ≈ cos(u)
+        @test Jacobi.dn(u,0) ≈ 1.
+        @test Jacobi.nn(u,0) ≈ 1.
+        @test Jacobi.cd(u,0) ≈ cos(u)
+        @test Jacobi.sd(u,0) ≈ sin(u)
+        @test Jacobi.nd(u,0) ≈ 1.
+        @test Jacobi.dd(u,0) ≈ 1.
+        @test Jacobi.dc(u,0) ≈ sec(u)
+        @test Jacobi.nc(u,0) ≈ sec(u)
+        @test Jacobi.sc(u,0) ≈ tan(u)
+        @test Jacobi.cc(u,0) ≈ 1.
+        @test Jacobi.ns(u,0) ≈ csc(u)
+        @test Jacobi.ds(u,0) ≈ csc(u)
+        @test Jacobi.cs(u,0) ≈ cot(u)
+        @test Jacobi.ss(u,0) ≈ 1.
 
-        @test am(u,1) ≈ atan(sinh(u))
-        @test sn(u,1) ≈ tanh(u)
-        @test cn(u,1) ≈ sech(u)
-        @test dn(u,1) ≈ sech(u)
-        @test nn(u,1) ≈ 1.
-        @test cd(u,1) ≈ 1.
-        @test sd(u,1) ≈ sinh(u)
-        @test nd(u,1) ≈ cosh(u)
-        @test dd(u,1) ≈ 1.
-        @test dc(u,1) ≈ 1.
-        @test nc(u,1) ≈ cosh(u)
-        @test sc(u,1) ≈ sinh(u)
-        @test cc(u,1) ≈ 1.
-        @test ns(u,1) ≈ coth(u)
-        @test ds(u,1) ≈ csch(u)
-        @test cs(u,1) ≈ csch(u)
-        @test ss(u,1) ≈ 1.
+        @test Jacobi.am(u,1) ≈ atan(sinh(u))
+        @test Jacobi.sn(u,1) ≈ tanh(u)
+        @test Jacobi.cn(u,1) ≈ sech(u)
+        @test Jacobi.dn(u,1) ≈ sech(u)
+        @test Jacobi.nn(u,1) ≈ 1.
+        @test Jacobi.cd(u,1) ≈ 1.
+        @test Jacobi.sd(u,1) ≈ sinh(u)
+        @test Jacobi.nd(u,1) ≈ cosh(u)
+        @test Jacobi.dd(u,1) ≈ 1.
+        @test Jacobi.dc(u,1) ≈ 1.
+        @test Jacobi.nc(u,1) ≈ cosh(u)
+        @test Jacobi.sc(u,1) ≈ sinh(u)
+        @test Jacobi.cc(u,1) ≈ 1.
+        @test Jacobi.ns(u,1) ≈ coth(u)
+        @test Jacobi.ds(u,1) ≈ csch(u)
+        @test Jacobi.cs(u,1) ≈ csch(u)
+        @test Jacobi.ss(u,1) ≈ 1.
     end
 
     @testset "errors" begin
         u = 0.5 # random value for testing
-        @test_throws DomainError am(u, -0.1)
-        @test_throws DomainError am(u, -eps(0.0))
+        @test_throws DomainError Jacobi.am(u, -0.1)
+        @test_throws DomainError Jacobi.am(u, -eps(0.0))
 
-        @test_throws DomainError am(u, 1 + eps(1.0))
-        @test_throws DomainError am(u, 1.1)
+        @test_throws DomainError Jacobi.am(u, 1 + eps(1.0))
+        @test_throws DomainError Jacobi.am(u, 1.1)
     end
 end
