0b7323c4b643d03f4e3e898c9e3078a315bee3ba
Mike Nolta <mike@nolta.net>
2020-02-08 13:59:14 -0500

    fix typo in 3d33f61acea458e8a5c57736043247fc3344633c

--- src/Elliptic.jl
+++ src/Elliptic.jl
@@ -42,8 +42,6 @@ E(phi::Real, m::Real) = E(Float64(phi), Float64(m))
 returns `(K(m), E(m))` for scalar `0 ≤ m ≤ 1`
 """
 function ellipke(m::Float64)
-
-function ellipke(m::Float64)
     if isnan(m) return NaN, NaN end
     if m < 0. || m > 1. throw(DomainError()) end
     if m == 1. return Inf, 1. end
